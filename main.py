8a5e730bc5ec0cb9233d7a1c66739c3a4f909936
alexnurin <an362@mail.ru>
2019-07-30 14:49:25 +0300

    bug fixed2

--- main.py
+++ main.py
@@ -95,7 +95,11 @@ def start_tags(message):
         return
     image_id = None
     for i in image_ids:
-        if (not already_check(id, i)) and (id != boss_id or len(open("tmp/tags/{}.txt".format(i), 'r', encoding='utf-8').readlines()) > 0):
+        if os.path.isfile("tmp/tags/{}.txt"):
+            olds = len(open("tmp/tags/{}.txt".format(i), 'r', encoding='utf-8').readlines()) > 0
+        else:
+            olds = 0
+        if (not already_check(id, i)) and (id != boss_id or olds > 0):
             image_id = i
             break
     if not image_id:
